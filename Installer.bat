64d2c47e4a466b962f31b2865dfe5584f7998e7e
Xanthus <Xanthus58@protonmail.com>
2022-02-25 02:14:58 +1030

    legacy removal

--- Installer.bat
+++
@@ -1,4 +0,0 @@
-winget install Python.Python.3
-pip install --upgrade pip
-pip install keyboard
-pip install colorama
--- Uninstaller.bat
+++
@@ -1,3 +0,0 @@
-REM Uninstalls all of the files needed for the affirmations program
-pip uninstall pip
-winget uninstall --name "python 3.10"
